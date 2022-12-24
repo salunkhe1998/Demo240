2d4e07bc8ae54fc1e0b6e7675c91a40b8bbc151c
Ashwini <salunkheashwini1998@gmail.com>
2022-12-24 10:41:20 +0530

    adding Username file

---
+++ Username.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWord
+{
+    public class Username
+    { 
+    int a;
+    public static void DisplayUsername()
+    {
+        Console.WriteLine("please enter the username :");
+        string username = Console.ReadLine();
+            if (username.Length > 3)
+            {
+                Console.WriteLine("Hello" + username + ",How are you ?");
+
+            }
+            else
+            {
+                Console.WriteLine("The username is not having 3 characters");
+            }
+        
+    }
+    
+    }
+}
