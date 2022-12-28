ece8b2314aae7fcd8a5c11ffb328d2d37aace28a
Ashwini <salunkheashwini1998@gmail.com>
2022-12-28 10:15:41 +0530

    This Is Folder

---
+++ FlipCoin.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWord
+{
+    public class FlipCoin
+    {
+        public static void CalculatePercentage()
+        {
+
+            int tailCount = 0;
+            int headCount = 0;
+            double headPercentage = 0.0;
+            double tailPercentage = 0.0;
+
+            Console.WriteLine("Please enter the value for number of flips");
+            int flip = Convert.ToInt32(Console.ReadLine());
+
+            if (flip > 0)
+            {
+                for (int i = 0; i < flip; i++)
+                {
+                    Random obje = new Random();
+                    int number = obje.Next(2);
+                    Console.WriteLine(number);
+                    if (number == 0)
+                    {
+                        tailCount++;
+                    }
+                    else
+                    {
+                        headCount++;
+                    }
+                }
+                Console.WriteLine("The TailCount is" + tailCount);
+                Console.WriteLine("The HeadCount is" + headCount);
+
+                headPercentage = (headCount * 100) / flip;
+                tailPercentage = (tailCount * 100) / flip;
+
+                Console.WriteLine("The tailPercentage is" + tailPercentage);
+                Console.WriteLine("The hailPercentage is" + tailPercentage);
+
+            }
+            else
+            {
+                Console.WriteLine("Please enter the correct number");
+            }
+        }
+       
+        }
+}
---
+++ Harmonic.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWord
+{
+    public class Harmonic
+    {
+        public static void HarmonicToN()
+        {
+            Console.WriteLine("Enter the power value:-");
+            int N = Convert.ToInt32(Console.ReadLine());
+            if(N >0)
+            {
+                for(int i=1; i<=N; i++)
+                {
+                    Console.Write("1/" + i + i+"+");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Enter number greater than 0.");
+            }
+        }
+    }
+}
